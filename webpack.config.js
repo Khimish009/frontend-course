4f7b7305ad73c8796d57e3cac06891154c77e770
Salman Khimishev <salman.khimishev@mail.ru>
2024-02-11 10:30:01 +0300

    feat: add tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,24 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build'),
         clean: true,
     },
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
