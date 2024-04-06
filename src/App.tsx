a0b37f00eba70d7eb03eff1d84e94851ce195ab8
Сальман Химишев <salman@MacBook-Pro-Salman.local>
2024-04-06 19:33:08 +0300

    feat: styles organizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Routes, Route, Link } from 'react-router-dom'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
 
+enum Theme {
+  Light = 'light',
+  Dark = 'dark'
+}
+
 export default function App() {
+  const [theme, setTheme] = useState<Theme>(Theme.Light)
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.Light ? Theme.Dark : Theme.Light)
+  }
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Change theme</button>
       <Link to={'/'}>Главная страница</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import './index.scss'
+import './styles/index.scss'
 
 render(
     <Router>
