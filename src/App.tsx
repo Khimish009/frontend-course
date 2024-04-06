4a190db3e434e7cc131bc7217af68bbaa52b92e7
Сальман Химишев <salman@MacBook-Pro-Salman.local>
2024-04-06 18:16:22 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Test } from "./components/Test";
+import { Routes, Route, Link } from 'react-router-dom'
+import { MainPage } from './pages/MainPage/MainPage'
+import { AboutPage } from './pages/AboutPage/AboutPage'
 
 export default function App() {
   return (
     <div className="app">
-      <Test />
+      <Link to={'/'}>Главная страница</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Routes>
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/' element={<MainPage />} />
+      </Routes>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import { render } from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.scss'
 
 render(
-    <App />,
+    <Router>
+      <App />
+    </Router>,
     document.querySelector('#root')
 )
