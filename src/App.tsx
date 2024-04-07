1c5b8bd9f7cd2cbce7be968b33b8cc59d9def52d
Сальман Химишев <salman@MacBook-Pro-Salman.local>
2024-04-07 19:09:29 +0300

    feat: create helper function classNames

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Suspense } from 'react'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
 import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classNames/classNames'
 
 export default function App() {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {selectable: true, red: false}, [theme])}>
       <button onClick={toggleTheme}>Change theme</button>
       <Link to={'/'}>Главная страница</Link>
       <Link to={'/about'}>О сайте</Link>
