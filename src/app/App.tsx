8c81281a0a543ebd0863ffc1be934af1c5a84765
Salman <himishevsa@intech.rshb.ru>
2024-04-12 19:02:40 +0300

    decomposition(navbar): create navbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,15 @@ import { Link } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from './providers/ThemeProvider'
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/navbar'
 
 export default function App() {
   const { theme, toggleTheme } = useTheme()
 
   return (
     <div className={classNames('app', {selectable: true, red: false}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>Change theme</button>
-      <Link to={'/'}>Главная страница</Link>
-      <Link to={'/about'}>О сайте</Link>
       <AppRouter />
     </div>
   )
