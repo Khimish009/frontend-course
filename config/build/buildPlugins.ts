a87bf40b68e9b4f1d8a2b186e5db11e37e809cc4
Сальман Химишев <himishevsa@intech.rshb.ru>
2024-09-10 17:32:25 +0300

    feat(webpack): add ReactRefreshWebpackPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
@@ -20,5 +21,6 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin(),
   ];
 }
