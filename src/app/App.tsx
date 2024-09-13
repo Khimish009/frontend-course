6286e2c00da9d11e1f7bd533f31c53c7ab4e1d5a
Сальман Химишев <himishevsa@intech.rshb.ru>
2024-09-13 14:03:01 +0300

    feat(*): update packages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,17 @@
-import { classNames } from 'shared/lib/classNames/classNames'
-import { useTheme } from './providers/ThemeProvider'
-import { AppRouter } from './providers/router'
-import { Navbar } from 'widgets/Navbar'
-import {Sidebar} from "widgets/Sidebar";
-import { Suspense } from 'react';
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
 
 export default function App() {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
 
   return (
-    <div className={classNames('app', {selectable: true, red: false}, [theme])}>
+    <div
+      className={classNames("app", { selectable: true, red: false }, [theme])}
+    >
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
@@ -18,5 +20,5 @@ export default function App() {
         </div>
       </Suspense>
     </div>
-  )
+  );
 }
