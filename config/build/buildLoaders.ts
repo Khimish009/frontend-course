b35dc5a8a4b4e78db973f6cb6e74faa50476d254
Сальман Химишев <himishevsa@intech.rshb.ru>
2024-09-13 13:28:11 +0300

    feat(*): add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,49 +7,63 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
       {
-        loader: 'file-loader',
+        loader: "file-loader",
       },
     ],
-  }
+  };
+
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        "plugins": [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true
+            }
+          ],
+        ],
+      },
+    },
+  };
 
   const svgLoader = {
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    }
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
 
-    const scssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-              loader: "css-loader",
-              options: {
-                modules: {
-                  auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                  localIdentName: isDev
-                    ? "[path][name]__[local]--[hash:base64:5]"
-                    : "[hash:base64:8]",
-                }
-              }
-            },
-            // Compiles Sass to CSS
-            "sass-loader",
-        ],
-    }
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
 
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
-
-    return [
-      fileLoader,
-      typescriptLoader,
-      scssLoader,
-      svgLoader,
-    ]
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, scssLoader];
 }
