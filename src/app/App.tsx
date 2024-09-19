994acedfbe20378f38677e601f5c3305b66dd35e
Сальман Химишев <himishevsa@intech.rshb.ru>
2024-09-19 18:09:07 +0300

    feat(*): add prettier and eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "./providers/ThemeProvider";
-import { AppRouter } from "./providers/router";
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
-import { Suspense } from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 export default function App() {
   const { theme } = useTheme();
 
   return (
     <div
-      className={classNames("app", { selectable: true, red: false }, [theme])}
+      className={classNames('app', { selectable: true, red: false }, [theme])}
     >
       <Suspense fallback="">
         <Navbar />
