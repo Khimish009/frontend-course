b88a1e423b4ce0b76bb74287ded6fe5906d453fb
Сальман Химишев <salman.khimishev01@gmail.com>
2024-09-21 13:57:14 +0300

    feat(*): add @babel/preset-typescript

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,7 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+});
