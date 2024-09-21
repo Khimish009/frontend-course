4de920c51cb4f7c43356a3e8a7749abd2a4eb119
Сальман Химишев <salman.khimishev01@gmail.com>
2024-09-21 18:21:54 +0300

    fear(ErrorBoundary): add ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import { Component, ErrorInfo, ReactElement } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactElement;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
