68ac4b72eb082aa24def8617033d91a10bdabe25
Сальман Химишев <salman.khimishev01@gmail.com>
2024-09-28 19:34:18 +0300

    feat(*): add ThemeSwitcher to storybook

---
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeSwitcher } from './ThemeSwitcher';
+
+const meta: Meta<typeof ThemeSwitcher> = {
+  title: 'shared/ThemeSwitcher',
+  component: ThemeSwitcher,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof ThemeSwitcher>;
+
+export const Light: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {},
+  decorators: [
+    Story => (
+      <div className="app dark">
+        <Story />
+      </div>
+    ),
+  ],
+};
